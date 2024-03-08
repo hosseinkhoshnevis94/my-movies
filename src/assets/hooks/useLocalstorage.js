91ae63550d32b4cd3b614f9d7764fbdc3bc48a01
Hossein <hossein.khoshnevis77@gmail.com>
2024-03-09 00:27:31 +0100

    fixed first load watched from localstorage bug

--- src/assets/hooks/useLocalstorage.js
+++ src/assets/hooks/useLocalstorage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(()=>JSON.parse(localStorage.getItem(key) ?? initialValue));
+  const [value, setValue] = useState(()=>JSON.parse(localStorage.getItem(key)) ?? initialValue);
   
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
